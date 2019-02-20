09074970cbe345cc74c4a473c4a361324e14d454
unknown <mjawwadiqbal22@gmail.com>
2019-02-20 10:27:18 -0800

    --Firebase Login

--- scripts/auth.js
+++ scripts/auth.js
@@ -14,5 +14,26 @@ auth.createUserWithEmailAndPassword(email,password).then(cred =>{
     const modal=document.querySelector('#modal-signup')
     M.Modal.getInstance(modal).close()
     signupForm.reset()
- })
+ });
+});
+//login 
+const loginForm=document.querySelector('#login-form');
+loginForm.addEventListener('submit',(e)=>{
+e.preventDefault();
+
+const email=loginForm['login-email'].value;
+const password=loginForm['login-password'].value;
+auth.signInWithEmailAndPassword(email,password).then(cred =>{
+console.log(cred.user)
+M.Modal.getInstance(modal).close()
+loginForm.reset();
+})
+})
+//logout
+const logout =document.querySelector('#logout');
+logout.addEventListener('click',(e)=>{
+    e.preventDefault();
+    auth.signOut().then(()=>{
+        console.log('User Logout')
+    })
 })
