0cfc783a8eb7df19c6b6fa9376abe479aecb7429
unknown <mjawwadiqbal22@gmail.com>
2019-02-23 01:38:14 -0800

    showing user info

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,17 @@
 const guideList = document.querySelector('.guides');
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
+const accountDetails =document.querySelector('.account-details');
+
 
 const setupUI = (user) => {
     console.log(user)
     if (user) {
+        //account info
+        const html=`
+        <div>Logged in as ${user.email}</div>
+        `;
+        accountDetails.innerHTML=html
       // toggle user UI elements
       loggedInLinks.forEach(item => item.style.display = 'block');
       loggedOutLinks.forEach(item => item.style.display = 'none');
