00f1297f929d264fd30e4f55b9e20928ac8b29ca
unknown <mjawwadiqbal22@gmail.com>
2019-02-23 01:19:20 -0800

    --onShanpshot Added

--- scripts/auth.js
+++ scripts/auth.js
@@ -5,7 +5,7 @@ auth.onAuthStateChanged(user=>{
 if(user){
     console.log(user)
     //get data
-db.collection('guides').get().then(snapshot=>{
+db.collection('guides').onSnapshot(snapshot=>{
     setupGuides(snapshot.docs);
     setupUI(user);
     
